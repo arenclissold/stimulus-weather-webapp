3b5970cf83fb2c4912ca8648703f9a669f2a4b26
Aren Clissold <arenclissold@gmail.com>
2022-06-06 22:45:10 +1000

    heroku changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 
 module.exports = {
   entry: path.resolve(__dirname, "./lib/index.js"),
-  mode: "development",
   output: {
     path: path.resolve(__dirname),
     filename: "main.js"
